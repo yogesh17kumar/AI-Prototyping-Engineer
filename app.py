2bf71e9af86a1f14c28a90e44b9993fbbaf1cbf7
yogesh17kumar <165543133+yogesh17kumar@users.noreply.github.com>
2026-01-29 20:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import streamlit as st
 from io import BytesIO
 from pypdf import PdfReader
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 
 # ================= CONFIG =================
